7a2765a2d2ee5dfbd3350788290a03691022d7b6
jun85664396 <jun85664396@gmail.com>
2017-07-08 06:30:01 +0000

    v0.0.5 release

--- lib/twkorean.rb
+++ lib/twkorean.rb
@@ -1,8 +1,3 @@
-# @name                twkorean-ruby
-# @author              JunSangPil
-# @version             0.0.4
-# @url                 https://github.com/jun85664396/twkorean-ruby
-# @license             Apache License 2.0
 require "twkorean/version"
 require 'rjb'
 require "twkorean/twitter_korean_text"
