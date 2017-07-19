c16607c1fb8ae5f510ea435af00e5b9d56989288
jun85664396 <jun85664396@gmail.com>
2017-07-19 17:07:42 +0000

    remove comment

--- spec/twkorean_spec.rb
+++ spec/twkorean_spec.rb
@@ -1,9 +1,3 @@
-# @name                twkorean-ruby
-# @author              JunSangPil
-# @version             0.0.4
-# @url                 https://github.com/jun85664396/twkorean-ruby
-# @license             Apache License 2.0
-
 RSpec.describe Twkorean do
   let(:twkorean) { Twkorean::TwitterKoreanText.new }
   let(:text) { twkorean.normalize("한국어를 처리하는 예시입니닼ㅋㅋㅋㅋㅋ #한국어") }
